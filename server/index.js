7a977615c272c9be0c15789519bbcce5a580278f
psubb <pranavsubbiah@gmail.com>
2025-06-18 15:08:28 -0700

    Created and tested db access via index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@
 require('dotenv').config({ path: './server/.env' });
 const express = require("express");
 const cors = require("cors");
+const db = require("./db")
 
 // using express
 const app = express();
@@ -16,6 +17,22 @@ app.use(express.json());
 
 app.get("/helloWorld", (req, res) => {
     res.json({ message: "Hello from Benny!"});
+});
+
+// testing DB access (lists out tables)
+app.get("/test-db", async(req, res) => {
+    try {
+        const result = await db.query(`
+            SELECT table_name
+            FROM information_schema.tables
+            WHERE table_schema = 'public'
+            ORDER BY table_name;
+            `);
+        res.json({tables: result.rows.map(row => row.table_name) });
+    } catch (err) {
+        console.error("DB error: ", err);
+        res.status(500).send("Database query failed");
+    }
 });
 
 app.listen(PORT, () => {
