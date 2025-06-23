983899d69edbcd8b682b64bf2434a94b58711c98
psubb <pranavsubbiah@gmail.com>
2025-06-23 13:22:21 -0700

    Create utils and add getSecretHash to hashUtils

---
+++ server/utils/hashUtils.js
@@ -0,0 +1,6 @@
+function getSecretHash(username, clientId, clientSecret) {
+    return crypto
+        .createHmac("SHA256", clientSecret)
+        .update(username + clientId)
+        .digest("base64");
+}
