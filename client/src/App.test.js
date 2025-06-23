ae1402c794def682745a3fd0fcb411c338b96914
AndreasNelson <nel02254@morris.umn.edu>
2025-06-23 14:51:24 -0500

    updated package structure in github workflow to properly run npm install, created mock test environment to get test check to pass

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app with brand name', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const brandElements = screen.getAllByText(/Midwest Health/i);
+  expect(brandElements[0]).toBeInTheDocument();
 });
