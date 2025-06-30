74bdd073bf77159f01e2521c870fe22656c16e94
psubb <pranavsubbiah@gmail.com>
2025-06-30 14:20:45 -0700

    Refactor verifyToken.js to use verifyJwt.js

--- server/middleware/verifyToken.js
+++ server/middleware/verifyToken.js
@@ -1,46 +1,21 @@
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
-const jwksClient = require("jwks-rsa");
+const verifyJwt = require('../utils/verifyJwt');
 
-const region = process.env.COGNITO_REGION;
-const userPoolId = process.env.COGNITO_USER_POOL_ID;
-
-const client = jwksClient({
-  jwksUri: `https://cognito-idp.${region}.amazonaws.com/${userPoolId}/.well-known/jwks.json`,
-});
-
-function getKey(header, callback) {
-  client.getSigningKey(header.kid, function (err, key) {
-    if (err) {
-      callback(err, null);
-    } else {
-      const signingKey = key.getPublicKey();
-      callback(null, signingKey);
-    }
-  });
-}
-
-module.exports = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
+module.exports = async (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
 
   if (!token) {
-    return res.status(401).json({ error: "Token missing from Authorization header" });
+    return res.status(401).json({ error: 'Token missing from Authorization header' });
   }
 
-  jwt.verify(
-    token,
-    getKey,
-    {
-      algorithms: ["RS256"],
-      issuer: `https://cognito-idp.${region}.amazonaws.com/${userPoolId}`,
-    },
-    (err, decoded) => {
-      if (err) {
-        return res.status(401).json({ error: "Token verification failed", details: err.message });
-      }
+  try {
+    const decoded = await verifyJwt(token);
 
-      req.user = decoded;
-      next();
-    }
-  );
+    // Attach decoded token to the request for use in controllers
+    req.user = decoded;
+
+    next();
+  } catch (err) {
+    console.error('Token verification failed:', err.message);
+    res.status(401).json({ error: 'Token verification failed', details: err.message });
+  }
 };
