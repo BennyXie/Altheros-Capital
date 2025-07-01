5625b1215c1a968a1b33a2586ca0182be8bb7730
sittingthyme <nicholan@andrew.cmu.edu>
2025-06-30 17:26:18 -0700

    Update symptoms.js

--- server/routes/symptoms.js
+++ server/routes/symptoms.js
@@ -1,3 +1,6 @@
+const express = require('express')
+const app = express()
+
 app.get('/symptoms', async (_req, res) => {
     const { rows } = await pool.query(`SELECT * FROM symptoms ORDER BY name`);
     res.json({ symptoms: rows });
