b790e44cc41b13e57bd6627031dc424057106945
sittingthyme <nicholan@andrew.cmu.edu>
2025-06-30 17:04:56 -0700

    Create symptoms.js

---
+++ server/routes/symptoms.js
@@ -0,0 +1,9 @@
+app.get('/symptoms', async (_req, res) => {
+    const { rows } = await pool.query(`SELECT * FROM symptoms ORDER BY name`);
+    res.json({ symptoms: rows });
+});
+  
+app.get('/languages', async (_req, res) => {
+    const { rows } = await pool.query(`SELECT * FROM languages ORDER BY name`);
+    res.json({ languages: rows });
+});
