994d01914eaaa97dffac03c4985735b98ad99d4a
psubb <pranavsubbiah@gmail.com>
2025-07-02 15:52:07 -0700

    Amend index.js to allow for websocket server

--- server/index.js
+++ server/index.js
@@ -2,14 +2,19 @@
 // define where to load environment variables
 require('dotenv').config({ path: './server/.env' });
 const express = require("express");
+const http = require("http");
 const cors = require("cors");
 const db = require("./db/pool")
 const authRoutes = require("./routes/authRoutes");
 const aiRoutes   = require("./routes/aiRoutes")
 const calendlyRoutes = require("./routes/calendlyRoute");
+const { initializeSocket } = require('./services/socketService');
 
 // using express
 const app = express();
+
+// enable socket.io 
+const server = http.createServer(app);
 
 // define port, stored in server/.env
 const PORT = process.env.PORT || 8080;
@@ -27,7 +32,10 @@ app.use("/ai", aiRoutes);
 // All calendlyRoutes start with /calendly
 app.use("/calendly", calendlyRoutes);
 
+// WebSocket installation
+initializeSocket(server);
 
-app.listen(PORT, () => {
+
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
