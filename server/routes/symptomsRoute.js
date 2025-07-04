84102c3ff515b0f50912d7aea4155cfaad2e6d0a
sittingthyme <nicholan@andrew.cmu.edu>
2025-07-03 21:02:20 -0700

    get /symptoms and /languages

--- server/routes/symptomsRoute.js
+++ server/routes/symptomsRoute.js
@@ -6,7 +6,3 @@ app.get('/symptoms', async (_req, res) => {
     res.json({ symptoms: rows });
 });
   
-app.get('/languages', async (_req, res) => {
-    const { rows } = await pool.query(`SELECT * FROM languages ORDER BY name`);
-    res.json({ languages: rows });
-});
